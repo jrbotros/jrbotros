8c4e225604bcce79673f3b224ebb0cb56e5c3bb9
Joseph Botros <jrbotros@gmail.com>
2016-09-13 18:16:09 -0400

    Cleanup scaffold

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,8 @@
   require('lib/normalize.css');
   require('lib/skeleton.css');
   require('scss/style.scss');
+
+  new Vivus('indiana', {duration: 200}, function(v) {
+    v.el.classList.add('done');
+  });
 })();
