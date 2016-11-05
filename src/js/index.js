e902df4978edfded71adeb13104ce3ee517eff18
Joseph Botros <jrbotros@gmail.com>
2016-11-05 18:13:57 -0400

    Add new personal logo

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,12 @@
   require('lib/skeleton.css');
   require('scss/style.scss');
 
-  new Vivus('indiana', {duration: 200}, function(v) {
-    v.el.classList.add('done');
+  // new Vivus('indiana', {duration: 200}, function(v) {
+  //   v.el.classList.add('done');
+  // });
+
+  new Vivus('logo', {
+    duration: 200,
+    animTimingFunction: Vivus.EASE
   });
 })();
