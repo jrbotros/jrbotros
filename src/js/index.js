28470a9f20a5eaea59c006f8c4f516dc385cbc36
Joseph Botros <jrbotros@gmail.com>
2016-11-29 08:48:23 -0500

    Redesign and add react

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,60 @@
-(function() {
-  'use strict';
+import 'lib/normalize.css';
+import 'lib/skeleton.css';
+import 'scss/style.scss';
 
-  // Stylesheets
-  require('lib/normalize.css');
-  require('lib/skeleton.css');
-  require('scss/style.scss');
+import moment from 'moment-timezone'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Vivus from 'vivus'
 
-  // new Vivus('indiana', {duration: 200}, function(v) {
-  //   v.el.classList.add('done');
-  // });
+import resume from 'assets/resume.json'
 
-  new Vivus('logo', {
-    duration: 200,
-    animTimingFunction: Vivus.EASE
-  });
-})();
+const prettyDate = date => moment(date).format('MMMM YYYY')
+const jobDateRange = job => {
+  let endDate = job.endDate ? prettyDate(job.endDate) : 'Present'
+  return <span className="resume-job__dateRange">{prettyDate(job.startDate)} – {endDate}</span>
+}
+
+class ResumeJob extends React.Component {
+  render () {
+    return (
+      <div className="resume-job">
+        <div className="resume-job__header">
+          <span className="resume-job__company">{this.props.job.company}</span>
+          {jobDateRange(this.props.job)}
+        </div>
+        <div className="resume-job__details">
+          <span className="resume-job__position">{this.props.job.position}</span>
+          <ul className="resume-job__highlights">
+            {this.props.job.highlights.map((highlight, i) => <li key={i}>{highlight}</li>)}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+class Resume extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>xp</h1>
+        <div className="resume-items">
+          {resume.work.map(job => <ResumeJob key={job.company + job.position} job={job} />)}
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Resume />,
+  document.getElementById('resume')
+);
+
+new Vivus('logo', {
+  duration: 200,
+  animTimingFunction: Vivus.EASE
+}, () => {
+  document.querySelector('.icon-github').classList.add('show')
+})
